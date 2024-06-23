bccc558f61b3d317e66aae5b3f203365c77e1de6
Adil Azhar <adil.azhar838@gmail.com>
2024-06-23 12:20:41 +0500

    Updated The Todos

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,17 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'minimal_todo_app.dart';
 
+// Todo : Modify The TodoController Notifier To AsyncNotifier class
+
 // Todo : Add Responsiveness So That It Keeps It Size Consistent On Different Screnss
 
 // Todo : Add Double Tap To Exit Fucntionality (Need To Test On Real Device)
 
 // Todo : Fix Highlight color on the TodoItem when editing
 
-// Todo : Modify The TodoController Notifier To AsyncNotifier class
+// Todo : Add Theme + Set The Text Styling
 
 // Todo : Add SplashScreen And write the initialization logic
-
-// Todo : Add Theme
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
